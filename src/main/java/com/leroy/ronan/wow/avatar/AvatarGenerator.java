4e890e28eefaabf1334a95bd5956ad2cac5d3228
Ronan <ronan@leroy.com>
2015-08-25 20:56:17 +0200

    Generate images one by one.

--- src/main/java/com/leroy/ronan/wow/avatar/AvatarGenerator.java
+++ src/main/java/com/leroy/ronan/wow/avatar/AvatarGenerator.java
@@ -2,7 +2,6 @@ package com.leroy.ronan.wow.avatar;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -10,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import javax.imageio.ImageIO;
 
@@ -26,6 +26,7 @@ import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -42,19 +43,22 @@ public class AvatarGenerator {
 
 	private Map<String, BufferedImage> cache = new ExpiringCache<>(TimeUnit.MILLISECONDS.convert(3, TimeUnit.HOURS));
 	
-	public BufferedImage buildImage(String region, String realm, String...characters) throws ClientProtocolException, IOException, URISyntaxException {
+	public BufferedImage buildImage(String region, String realm, String...characters) {
 		String key = buildKey(region, realm, characters);
 		BufferedImage res = cache.get(key);
 		if (res == null) {
-			if (characters.length <= 5){
-	            URL url = getImgUrl(region, realm, characters);
-	            res = ImageIO.read(url);
-	        } else {
-	        	res = ImageCombinator.combineLeftToRight(
-	    				buildImage(region, realm, Arrays.copyOfRange(characters, 0, 4)),
-	    				buildImage(region, realm, Arrays.copyOfRange(characters, 4, characters.length))
-	    			);
-	        }
+			if (characters.length > 1) {
+				res = Stream.of(characters)
+						.map(c -> buildImage(region, realm, c))
+						.reduce(ImageCombinator::combineLeftToRight)
+						.orElse(null);
+			} else {
+				try {
+					res = getImg(region, realm, characters[0]);
+				} catch (URISyntaxException | IOException e) {
+					log.error(e.getMessage(), e);
+				}
+			}
 			cache.put(key, res);
 		}
         return res;
@@ -64,27 +68,23 @@ public class AvatarGenerator {
 		return region+","+realm+","+StringUtils.join(characters, "-");
 	}
 
-	private URL getImgUrl(String region, String realm, String... characters)
-			throws IOException, ClientProtocolException, URISyntaxException, MalformedURLException {
-		log.debug("getImgUrl("+region+","+realm+","+StringUtils.join(characters, ",")+")");
-		
-		URL url;
+	private BufferedImage getImg(String region, String realm, String character) throws ClientProtocolException, URISyntaxException, IOException {
+		log.debug("getImgUrl("+region+","+realm+","+character+")");
+
+		BufferedImage res = null;
         BasicCookieStore cookieStore = new BasicCookieStore();
-        CloseableHttpClient httpclient = HttpClients.custom()
-                .setDefaultCookieStore(cookieStore)
-                .build();
-		try {
-			for (String character : characters){
-				this.get(cookieStore, httpclient, "http://www.best-signatures.com/api/?region="+region+"&realm="+realm+"&char="+character+"&type=Sign9&preview=1&c1=class");
-			}
+        HttpClientBuilder builder = HttpClients.custom().setDefaultCookieStore(cookieStore);
+        
+		try (CloseableHttpClient httpclient = builder.build()){
+			this.get(cookieStore, httpclient, "http://www.best-signatures.com/api/?region="+region+"&realm="+realm+"&char="+character+"&type=Sign9&preview=1&c1=class");
 			
-			get(cookieStore, httpclient, "http://www.best-signatures.com/wow/");
+			this.get(cookieStore, httpclient, "http://www.best-signatures.com/wow/");
 	        
 	        this.post(httpclient, cookieStore, 
 	        		"http://www.best-signatures.com/ajax/generator/load/", 
 	                new BasicNameValuePair("region", region),
 	                new BasicNameValuePair("server", realm),
-	                new BasicNameValuePair("char", StringUtils.join(characters, ",")),
+	                new BasicNameValuePair("char", character), // Multi : StringUtils.join(characters, ","))
 	                new BasicNameValuePair("lang", "en_GB")
 	        		);
 	        this.post(httpclient, cookieStore, 
@@ -105,25 +105,20 @@ public class AvatarGenerator {
 	        		"http://www.best-signatures.com/ajax/generator/save/", 
 	        		new BasicNameValuePair("save", "1")
 	        		);
-		
 	        JSONObject obj = (JSONObject)JSONValue.parse(json);
-
-	        
 	        String link = StringEscapeUtils.unescapeEcmaScript((String)obj.get("link"));
+	        
 	        log.debug("link:"+link);
-	        url = new URL(link);
-		} finally {
-	        httpclient.close();
-	    }
-		return url;
+	        res = ImageIO.read(new URL(link));
+		}
+		return res;
 	}
 
 	private String get(BasicCookieStore cookieStore, CloseableHttpClient httpclient, String uri) throws IOException, ClientProtocolException {
 		String res = null;
 		HttpGet get = new HttpGet(uri);
-		CloseableHttpResponse response1 = httpclient.execute(get);
-		try {
-		    HttpEntity entity = response1.getEntity();
+		try (CloseableHttpResponse response = httpclient.execute(get)) {
+		    HttpEntity entity = response.getEntity();
 
 		    res = EntityUtils.toString(entity);
 		    EntityUtils.consume(entity);
@@ -137,8 +132,6 @@ public class AvatarGenerator {
 		            log.debug("- " + cookies.get(i).toString());
 		        }
 		    }
-		} finally {
-		    response1.close();
 		}
 		return res;
 	}
@@ -150,8 +143,7 @@ public class AvatarGenerator {
         		.setEntity(new UrlEncodedFormEntity(Arrays.asList(params), Charset.forName("UTF-8")))
         		.build();
         
-        CloseableHttpResponse response = httpclient.execute(post);
-        try {
+        try (CloseableHttpResponse response = httpclient.execute(post)) {
             HttpEntity entity = response.getEntity();
 
 		    res = EntityUtils.toString(entity);
@@ -166,8 +158,6 @@ public class AvatarGenerator {
                     log.debug("- " + cookies.get(i).toString());
                 }
             }
-        } finally {
-            response.close();
         }
 		return res;
 	}
